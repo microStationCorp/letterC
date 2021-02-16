473f8ad285350b9f787879b54fb034e690cac01e
sherlock <sherlockcreation@gmail.com>
2021-02-16 21:39:37 +0530

    type interface adding test

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,14 @@
-import { CharacterInterface } from "./interfaces";
-
 declare module "letterc" {
   export function sum(num1: number, num2: number): number;
   export function wordCount(text: string): CharacterInterface[];
+  export interface CharacterInterface {
+    char: string | undefined;
+    num: number;
+    symbol?: string;
+  }
+
+  export interface SymbolInterface {
+    name: string;
+    symbol: string;
+  }
 }
